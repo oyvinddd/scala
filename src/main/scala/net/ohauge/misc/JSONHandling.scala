133633c2f449cd5ea4a51dfb1ef65cc4a1cb2913
Øyvind <oyvind.hauge@evry.com>
2021-06-15 17:55:40 +0200

    ...

--- src/main/scala/net/ohauge/misc/JSONHandling.scala
+++ src/main/scala/net/ohauge/misc/JSONHandling.scala
@@ -1,68 +1,49 @@
 package net.ohauge.misc
 
-import scala.io.Source
-import io.circe.generic.auto._
-import io.circe.parser._
-import net.ohauge.spark.Utils._
 import org.apache.spark.sql.{Encoders, SparkSession}
+import net.ohauge.spark.Utils._
 import sttp.client3._
+
+import java.sql.Timestamp
 
 case class ContactDetails(firstName: Option[String], lastName: Option[String], address1: Option[String], address2: Option[String],
                           address3: Option[String], zip: Option[String], country: Option[String], phone: Option[String],
                           email: Option[String], company: Option[String], nationality: Option[String])
 
-case class Data(reference: String, totalSalesPriceIncVat: Option[Int], totalSalesPriceExVat: Option[Double], totalSalesVat: Option[Double],
-                totalProfit: Option[Int], totalExternalCommission: Option[Int], totalMarkup: Option[Int], paymentRef: Option[String],
-                orderStatus: Option[String], productSalesChannel: Option[ProductSalesChannel], contactDetails: Option[ContactDetails],
-                orderLines: Option[Seq[OrderLines]], passengers: Option[Seq[Passengers]], createdDate: Option[String],
-                updatedDate: Option[String])
+case class Data(reference: String, totalSalesPriceIncVat: Option[Double], totalSalesPriceExVat: Option[Double], totalSalesVat: Option[Double],
+                totalProfit: Option[Double], totalExternalCommission: Option[Double], totalMarkup: Option[Double], paymentRef: Option[String],
+                paymentFee: Option[Double], orderStatus: Option[String], productSalesChannel: Option[ProductSalesChannel], contactDetails: Option[ContactDetails],
+                orderLines: Option[Seq[OrderLines]], passengers: Option[Seq[Passengers]], createdDate: Option[Timestamp],
+                updatedDate: Option[Timestamp])
 
 case class OrderLineDetails(id: Option[Int], externalInstanceId: Option[String], externalProductKey: Option[String],
                             externalSourceSystem: Option[String], externalSupplier: Option[String], externalStatus: Option[String],
-                            externalProductName: Option[String], externalBookingRef: Option[String], externalCommission: Option[Int],
-                            inventoryReservationId: Option[String], mandatory: Option[Boolean], salesPriceIncVat: Option[Int],
+                            externalProductName: Option[String], externalBookingRef: Option[String], externalCommission: Option[Double],
+                            inventoryReservationId: Option[String], mandatory: Option[Boolean], salesPriceIncVat: Option[Double],
                             salesPriceExVat: Option[Double], totalSalesVat: Option[Double], start: Option[String], end: Option[String],
-                            orderLineDetailsPrices: Seq[OrderLineDetailsPrices], profit: Option[Int], markup: Option[Int])
+                            orderLineDetailsPrices: Seq[OrderLineDetailsPrices], profit: Option[Double], markup: Option[Double])
 
 case class OrderLineDetailsPrices(vatRateDescription: Option[String], priceType: Option[String], totalAmountIncVat: Option[Double],
                                   totalAmountExVat: Option[Double], vatRate: Option[Double], totalVatAmount: Option[Double],
                                   ticketType: Option[String], ticketQuantity: Option[Int], refund: Option[String])
 
 case class OrderLines(id: Option[Int], productPackageVersionId: Option[String], productId: Option[Int], productName: Option[String],
-                      salesPriceIncVat: Option[Int], salesPriceExVat: Option[Double], totalSalesVat: Option[Double],
-                      totalProfit: Option[Int], totalExternalCommission: Option[Int], totalMarkup: Option[Int],
+                      salesPriceIncVat: Option[Double], salesPriceExVat: Option[Double], totalSalesVat: Option[Double],
+                      totalProfit: Option[Double], totalExternalCommission: Option[Double], totalMarkup: Option[Double],
                       orderLineDetails: Option[Seq[OrderLineDetails]])
 
-case class Passengers(firstName: Option[String], lastName: Option[String], dateOfBirth: Option[String], nationality: Option[String],
+case class Passengers(firstName: Option[String], lastName: Option[String], dateOfBirth: Option[Timestamp], nationality: Option[String],
                       passportNumber: Option[String], age: Option[Int])
 
-case class ProductSalesChannel(id: Option[Int], periodName: Option[String], fromDate: Option[String], toDate: Option[String],
+case class ProductSalesChannel(id: Option[Int], periodName: Option[String], fromDate: Option[Timestamp], toDate: Option[Timestamp],
                                productSalesChannelCategories: Option[Seq[String]], salesChannel: Option[String])
 
 case class RootInterface(pageNumber: Option[Int], pageSize: Option[Int], firstPage: Option[String], lastPage: Option[String],
                          totalPages: Option[Int], totalRecords: Option[Int], nextPage: Option[String], previousPage: Option[String],
                          data: Option[Seq[Data]])
 
-// for testing purposes only
-case class Player(name: Option[String], age: Option[Int])
-case class Team(name: Option[String], players: Option[Seq[Player]])
-
-object JSONHandling extends App {
-    // setup and initialize spark session object
-    val spark = SparkSession.builder().master("local[1]").appName("JSONHandling").getOrCreate()
-    import spark.implicits._
-
-    val json: String = Source.fromFile("./response.json").mkString
-
-    val decoded = decode[RootInterface](json) match {
-        case Right(rootInterface) => rootInterface
-        case Left(error) => error
-    }
-
-    println(decoded)
-}
-
 object SparkJSONHandling extends App {
+
     val spark = SparkSession.builder().master("local[1]").appName("SparkJSONHandling").getOrCreate()
     import spark.implicits._
 
@@ -70,34 +51,33 @@ object SparkJSONHandling extends App {
 
     val backend = HttpURLConnectionBackend()
     val response = request.send(backend)
-    println(response)
 
     val rootSchema = Encoders.product[RootInterface].schema
 
     val df = spark.read
       .option("multiline", true)
       .schema(rootSchema)
-      .json("./response.json")
+      .json("./response2.json")
       .as[RootInterface]
       .toDF()
 
     val orderDF = df.transform(structListColumnToDataFrame("data"))
 
-    val contactDF = orderDF.transform(structColumnToDataFrame("contactDetails", "reference"))
+    val contactDF = orderDF.transform(structColumnToDataFrame("contactDetails", "frg_", "reference"))
 
-    val orderLinesDF = orderDF.transform(structListColumnToDataFrame("orderLines", "reference"))
+    orderDF.transform(structColumnToDataFrame2("contactDetails", Some("reference"), Some("id_"))).show()
 
-    val orderLineDetailsDF = orderLinesDF.transform(structListColumnToDataFrame("orderLineDetails"))
+    //val orderLinesDF = orderDF.transform(structListColumnToDataFrame("orderLines", "reference"))
 
-    val pricesDF = orderLineDetailsDF.transform(structListColumnToDataFrame("orderLineDetailsPrices"))
+    //val orderLineDetailsDF = orderLinesDF.transform(structListColumnToDataFrame("orderLineDetails"))
 
-    val productSalesChannelDF = orderDF.transform(structColumnToDataFrame("productSalesChannel"))
+    //val pricesDF = orderLineDetailsDF.transform(structListColumnToDataFrame("orderLineDetailsPrices"))
 
+    //val productSalesChannelDF = orderDF.transform(structColumnToDataFrame("productSalesChannel"))
 
-
-    orderDF.show()
+    //orderDF.show()
     contactDF.show()
-    orderLinesDF.show()
-    orderLineDetailsDF.show()
-    productSalesChannelDF.show()
+    //orderLinesDF.show()
+    //orderLineDetailsDF.show()
+    //productSalesChannelDF.show()
 }
--- src/main/scala/net/ohauge/spark/Utils.scala
+++ src/main/scala/net/ohauge/spark/Utils.scala
@@ -1,8 +1,9 @@
 package net.ohauge.spark
 
 import org.apache.orc.impl.ConvertTreeReaderFactory.DateFromStringGroupTreeReader
-import org.apache.spark.sql.functions.{col, column, explode}
+import org.apache.spark.sql.functions.{col, column, explode, lit}
 import org.apache.spark.sql.{Column, DataFrame}
+import org.glassfish.jersey.internal.util.collection.StringIgnoreCaseKeyComparator
 
 object Utils {
 
@@ -16,10 +17,20 @@ object Utils {
         df.filter(!col(colName).isin(values:_*))
     }
 
-    def structColumnToDataFrame(colName: String, additionalColNames: String*)(df: DataFrame): DataFrame = {
+    def structColumnToDataFrame(colName: String, prefix: String, additionalColNames: String*)(df: DataFrame): DataFrame = {
         val allColNames: Seq[String] = additionalColNames :+ s"${colName}.*"
         val allCols: Seq[Column] = allColNames.map(col(_))
         df.withColumn(colName, col(colName)).select(allCols:_*)
+    }
+
+    def structColumnToDataFrame2(colName: String, idColName: Option[String], idColPrefix: Option[String])(df: DataFrame): DataFrame = {
+        if(!idColName.isEmpty) {
+            val newIDColName = s"${idColPrefix.get}${idColName.getOrElse("")}"
+            val allColumns: Seq[Column] = Array(s"$colName.*", newIDColName).map(col(_))
+            df.withColumn(newIDColName, col(idColName.get)).withColumn(colName, col(colName)).select(allColumns:_*)
+        } else {
+            df.withColumn(colName, col(colName)).select(s"$colName.*")
+        }
     }
 
     def structListColumnToDataFrame(colName: String, additionalColNames: String*)(df: DataFrame): DataFrame = {
